5f564d710dc1a2e2492f77b70c6c9df30ab5b7da
a0v00fg <anmol.vijayvargiya@walmartlabs.com>
2019-02-18 18:30:36 -0800

    Add a bare bone media page

---
+++ src/pages/media/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const MediaPage = () => {
+  return (
+    <Layout>
+      <h1>MEDIA PAGE</h1>
+    </Layout>
+  )
+}
+
+export default MediaPage
