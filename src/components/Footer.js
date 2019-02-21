f0d0f77ae93b533a58647260690c7074b2ea4090
a0v00fg <anmol.vijayvargiya@walmartlabs.com>
2019-02-20 20:49:27 -0800

    added custom footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,115 +1,57 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import styled from 'styled-components'
+import BuildIcon from '@material-ui/icons/Build'
+import BrushIcon from '@material-ui/icons/Brush'
+import { withStyles } from '@material-ui/core/styles'
+import red from '@material-ui/core/colors/red'
+import { relative } from 'path'
 
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+const styles = theme => ({
+  icon: {
+    color: red[800],
+    position: relative,
+    bottom: '-30px',
+  },
+})
 
-const Footer = class extends React.Component {
-  render() {
-    return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-    )
+const Container = styled.div`
+  margin: auto;
+`
+const Divider = styled.hr`
+  width: 80%;
+`
+const Content = styled.div`
+  text-align: center;
+  padding-top: 20px;
+  padding-bottom: 40px;
+`
+
+const LinkedInLink = styled.a`
+  color: inherit;
+  padding: 10px;
+  &:hover {
+    color: inherit;
   }
-}
+`
 
-export default Footer
+const Footer = ({ classes }) => (
+  <Container>
+    <Divider />
+    <Content>
+      <LinkedInLink
+        href="https://www.linkedin.com/in/anmolvijayvargiya"
+        target="_blank"
+      >
+        Anmol Vijayvargiya <BuildIcon className={classes.icon} />
+      </LinkedInLink>
+      <LinkedInLink
+        href="https://www.linkedin.com/in/asha-latha-amara"
+        target="_blank"
+      >
+        Asha Latha Amara <BrushIcon className={classes.icon} />
+      </LinkedInLink>
+    </Content>
+  </Container>
+)
+
+export default withStyles(styles)(Footer)
