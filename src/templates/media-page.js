100b810f0a9317302b2570ab52e5f3674b9d78fe
a0v00fg <anmol.vijayvargiya@walmartlabs.com>
2019-02-27 20:38:45 -0800

    commit intermediate work

--- src/templates/media-page.js
+++ src/templates/media-page.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import Layout from '../components/Layout'
 import YoutubePlayer from '../components/YouTube'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  @media (max-width: 1300px) {
+    flex-direction: row;
+    justify-content: center;
+  }
+`
+const VideoContainer = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
@@ -16,20 +30,71 @@ const Container = styled.div`
   }
 `
 
-export const MediaPageTemplate = ({ page }) => (
-  <Container className="section section--gradient">
-    {page.frontmatter.youtubeVideos.map(({ title, description, url }) => (
-      <YoutubePlayer title={title} description={description} url={url} />
-    ))}
-  </Container>
-)
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: 'green',
+  },
+})
 
-const MediaPage = ({ data }) => {
-  const { markdownRemark: page } = data
+export class MediaPageTemplate extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 'interview',
+    }
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value: value })
+  }
+
+  render() {
+    const { classes } = this.props
+    const { value } = this.state
+
+    return (
+      <Container className="section section--gradient">
+        <Tabs
+          value={value}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={this.handleChange}
+          classes={classes.root}
+        >
+          <Tab value="interview" label="Interview" />
+          <Tab value="youtube" label="Youtube" />
+        </Tabs>
+        {this.state.value === 'interview' ? (
+          <div>This is Interviews</div>
+        ) : (
+          <VideoContainer>
+            {this.props.page.frontmatter.youtubeVideos.map(
+              ({ title, description, url }) => (
+                <YoutubePlayer
+                  title={title}
+                  description={description}
+                  url={url}
+                />
+              )
+            )}
+          </VideoContainer>
+        )}
+      </Container>
+    )
+  }
+}
+
+const MediaPage = props => {
+  const { markdownRemark: page } = props.data
+  const { classes } = props
 
   return (
     <Layout>
-      <MediaPageTemplate page={{ ...page, bodyIsMarkdown: false }} />
+      <MediaPageTemplate
+        page={{ ...page, bodyIsMarkdown: false }}
+        classes={classes}
+      />
     </Layout>
   )
 }
@@ -37,8 +102,7 @@ const MediaPage = ({ data }) => {
 MediaPage.propTypes = {
   data: PropTypes.object.isRequired,
 }
-
-export default MediaPage
+export default withStyles(styles)(MediaPage)
 
 export const mediaPageQuery = graphql`
   query MediaPage($id: String!) {
