63736943a5074cf356b51df7da9efab68116c2db
a0v00fg <anmol.vijayvargiya@walmartlabs.com>
2019-03-06 16:21:43 -0800

    Check for document existance and pass default empty object to  StyleSheetManager

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -10,9 +10,6 @@ import { StyleSheetManager } from 'styled-components'
 
 import './layout.sass'
 
-const iframe = document.getElementsByTagName('iframe')[0]
-const iframeHeadElem = iframe.contentDocument.head
-
 const SiteContainer = styled.div`
   margin: 0;
   padding: 0;
@@ -57,64 +54,72 @@ const TemplateWrapper = ({ children }) => (
         }
       }
     `}
-    render={data => (
-      <StyleSheetManager target={iframeHeadElem}>
-        <GlobalTheme>
-          <SiteContainer>
-            <Helmet>
-              <html lang="en" />
-              <title>{data.site.siteMetadata.title}</title>
-              <meta
-                name="description"
-                content={data.site.siteMetadata.description}
-              />
+    render={data => {
+      const iframe =
+        typeof document !== 'undefined' &&
+        document.getElementsByTagName('iframe')[0]
+      const iframeHeadElem =
+        typeof document !== 'undefined' && iframe.contentDocument.head
+      console.log('I FRAME HEAD ELEMENT :::: ', iframeHeadElem)
+      return (
+        <StyleSheetManager target={iframeHeadElem || {}}>
+          <GlobalTheme>
+            <SiteContainer>
+              <Helmet>
+                <html lang="en" />
+                <title>{data.site.siteMetadata.title}</title>
+                <meta
+                  name="description"
+                  content={data.site.siteMetadata.description}
+                />
 
-              <link
-                rel="apple-touch-icon"
-                sizes="180x180"
-                href="/img/apple-touch-icon.png"
-              />
-              <link
-                rel="icon"
-                type="image/png"
-                href="/img/favicon-32x32.png"
-                sizes="32x32"
-              />
-              <link
-                rel="icon"
-                type="image/png"
-                href="/img/favicon-16x16.png"
-                sizes="16x16"
-              />
+                <link
+                  rel="apple-touch-icon"
+                  sizes="180x180"
+                  href="/img/apple-touch-icon.png"
+                />
+                <link
+                  rel="icon"
+                  type="image/png"
+                  href="/img/favicon-32x32.png"
+                  sizes="32x32"
+                />
+                <link
+                  rel="icon"
+                  type="image/png"
+                  href="/img/favicon-16x16.png"
+                  sizes="16x16"
+                />
 
-              <link
-                rel="mask-icon"
-                href="/img/safari-pinned-tab.svg"
-                color="#ff4400"
-              />
-              <meta name="theme-color" content="#fff" />
+                <link
+                  rel="mask-icon"
+                  href="/img/safari-pinned-tab.svg"
+                  color="#ff4400"
+                />
+                <meta name="theme-color" content="#fff" />
 
-              <meta property="og:type" content="business.business" />
-              <meta
-                property="og:title"
-                content={data.site.siteMetadata.title}
-              />
-              <meta property="og:url" content="/" />
-              <meta property="og:image" content="/img/og-image.jpg" />
-              <link
-                href="https://fonts.googleapis.com/css?family=Nunito+Sans"
-                rel="stylesheet"
-              />
-            </Helmet>
-            <Body>
-              <Header title={data.site.siteMetadata.title} />
-              {children}
-              <Footer />
-            </Body>
-          </SiteContainer>
-        </GlobalTheme>
-      </StyleSheetManager>
-    )}
+                <meta property="og:type" content="business.business" />
+                <meta
+                  property="og:title"
+                  content={data.site.siteMetadata.title}
+                />
+                <meta property="og:url" content="/" />
+                <meta property="og:image" content="/img/og-image.jpg" />
+                <link
+                  href="https://fonts.googleapis.com/css?family=Nunito+Sans"
+                  rel="stylesheet"
+                />
+              </Helmet>
+              <Body>
+                <Header title={data.site.siteMetadata.title} />
+                {children}
+                <Footer />
+              </Body>
+            </SiteContainer>
+          </GlobalTheme>
+        </StyleSheetManager>
+      )
+    }}
   />
 )
 
