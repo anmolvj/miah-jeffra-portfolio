f696df6e00f51b9f2ad6f8b4da0a7a37f04ce6fc
a0v00fg <anmol.vijayvargiya@walmartlabs.com>
2019-03-17 21:37:37 -0700

    Remove paper compnent from form

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -3,27 +3,30 @@ import styled from 'styled-components'
 import Layout from '../../components/Layout'
 import FormikForm from '../../components/FormikForm'
 import FancyMap from '../../img/contact-map.png'
-import Paper from '@material-ui/core/Paper'
 
-const Container = styled(Paper)`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+const Container = styled.div`
+  border-radius: 3px;
+  overflow: hidden;
+  width: 50%;
+  margin: auto;
 `
 
 const FormContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  width: 50%;
+  background-color: rgb(204, 204, 204, 0.3);
   margin: auto;
   @media (max-width: 700px) {
     width: 100%;
   }
 `
 
-const Tiltle = styled.h2`
+const Tiltle = styled.div`
   text-align: center;
+  padding: 10px;
+  font-family: ${props => props.theme.font.family.secondary};
+  font-weight: bold;
 `
 
 const Map = styled.img`
