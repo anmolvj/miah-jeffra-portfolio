f1151914b8cf90ae3043069155a5325c4cfc4f0d
a0v00fg <anmol.vijayvargiya@walmartlabs.com>
2019-03-31 20:31:31 -0700

    First pass at top nav styling

--- src/components/Header/TopNavbar/index.js
+++ src/components/Header/TopNavbar/index.js
@@ -3,7 +3,14 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 // import './styles-topnav.sass'
 
-const Container = styled.div``
+const Container = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+  margin-bottom: 10px;
+  font-size: 12px;
+  font-weight: bold;
+`
 
 const isActive = ({ isCurrent }) => {
   return isCurrent
@@ -17,12 +24,15 @@ export default () => {
       <Link className="" getProps={isActive} to="/about">
         About
       </Link>
+
       <Link className="" getProps={isActive} to="/publication">
         Publication
       </Link>
+
       <Link className="" getProps={isActive} to="/media">
         Media
       </Link>
+
       <Link className="" getProps={isActive} to="/contact">
         Contact
       </Link>
