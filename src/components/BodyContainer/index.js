623cd08beda9123e8b77e8340b4134e1d185f30e
a0v00fg <anmol.vijayvargiya@walmartlabs.com>
2019-03-31 19:14:20 -0700

    fix width

--- src/components/BodyContainer/index.js
+++ src/components/BodyContainer/index.js
@@ -5,12 +5,14 @@ import Paper from '@material-ui/core/Paper'
 const BodyContainer = styled(Paper)`
   display: ${props => (Boolean(props.hidden) ? 'hidden' : 'block')};
   font-family: ${props => props.theme.font.family.secondary};
-  width: 90%;
   margin: auto;
   font-size: 12px;
   padding: 10px;
   margin-bottom: 20px;
   max-width: 650px;
+  @media (max-width: 750px) {
+    width: 90%;
+  }
 `
 
 export default ({ children, hidden }) => (
