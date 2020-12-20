3cba2fb5efd97ff29d9619e766d094d81c870260
Anmol Vijayvargiya <anmol.vijay94@gmail.com>
2020-12-20 15:35:00 -0800

    Nuke unused code

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,17 +1,13 @@
-import CMS from 'netlify-cms-app'
+import CMS from 'netlify-cms-app';
 
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BookPagePreview from './preview-templates/BookPagePreview'
-import MediaPagePreview from './preview-templates/MediaPagePreview'
-import ContactPagePreview from './preview-templates/ContactPagePreview'
+import IndexPagePreview from './preview-templates/IndexPagePreview';
+import AboutPagePreview from './preview-templates/AboutPagePreview';
+import BookPagePreview from './preview-templates/BookPagePreview';
+import MediaPagePreview from './preview-templates/MediaPagePreview';
+import ContactPagePreview from './preview-templates/ContactPagePreview';
 
-CMS.registerPreviewTemplate('book', BookPagePreview)
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
-CMS.registerPreviewTemplate('media', MediaPagePreview)
-CMS.registerPreviewTemplate('contact', ContactPagePreview)
+CMS.registerPreviewTemplate('book', BookPagePreview);
+CMS.registerPreviewTemplate('index', IndexPagePreview);
+CMS.registerPreviewTemplate('about', AboutPagePreview);
+CMS.registerPreviewTemplate('media', MediaPagePreview);
+CMS.registerPreviewTemplate('contact', ContactPagePreview);
